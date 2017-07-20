c6ff21f6223d1b954c3887cb7ee663dcf58942c8
Roddur Dasgupta <roddurd@gmail.com>
2017-07-20 14:29:31 -0700

    Add files via upload
    Error handling

--- Program.cs
+++ Program.cs
@@ -27,16 +27,19 @@ namespace GradDesc
             {
                 foreach (string line in File.ReadLines(path))
                 {
-                    char[] del = { ',', ' ', '\t' };
-                    string[] vals = line.Split(del);
-                    Xs.Add(Convert.ToDouble(vals[0].Trim()));
-                    Ys.Add(Convert.ToDouble(vals[1].Trim()));
+                    if (!line.Substring(0, 1).Equals("*")) //for comments
+                    {
+                        char[] del = { ',', ' ', '\t' };
+                        string[] vals = line.Split(del);
+                        Xs.Add(Convert.ToDouble(vals[0].Trim()));
+                        Ys.Add(Convert.ToDouble(vals[1].Trim()));
+                    }
                 }
             }
-            catch (FileNotFoundException e)
+            catch (FileNotFoundException fnfe)
             {
                 Console.WriteLine("File not found. Please check your file path. Error details:");
-                Console.WriteLine(e.Message);
+                Console.WriteLine(fnfe.Message);
                 Console.WriteLine("Press enter to try again.");
                 Console.ReadLine();
                 Console.Clear();
@@ -44,13 +47,32 @@ namespace GradDesc
             }
             catch (FormatException fe)
             {
-                Console.WriteLine("Oops! Looks like your data may not be formatted correctly.\nX and Y values must be separated by either a comma, space, OR tab (cannot be more than one).");
+                Console.WriteLine("Oops! Looks like your data may not be formatted correctly.\nX and Y values must be separated by either a comma, space, OR tab (cannot be more than one).\nFull error details:");
                 Console.WriteLine(fe.Message);
                 Console.WriteLine("Press enter when you're ready to try again.");
                 Console.ReadLine();
                 Console.Clear();
                 Setup();
             }
+            catch (DirectoryNotFoundException dnfe)
+            {
+                Console.WriteLine("Oops! The directory was not found. Perhaps there is a typo in your file path?\nHere is the full error message: ");
+                Console.WriteLine(dnfe.Message);
+                Console.WriteLine("Press enter when you're ready to try again.");
+                Console.ReadLine();
+                Console.Clear();
+                Setup();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Okay I'm usually good with exceptions but I have no idea what you messed up.\nHere, Microsoft will tell you what you did wrong:");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press enter when you're ready to try again.");
+                Console.ReadLine();
+                Console.Clear();
+                Setup();
+            }
+            
             Console.Write("Data successfully parsed.\nChoose a learning rate [You probably shouldn't exceed 0.01]: ");
             string response = Console.ReadLine();
             while (!double.TryParse(response, out alpha))
@@ -58,12 +80,32 @@ namespace GradDesc
                 Console.Write("Input was not a double. Try again, choose a learning rate: ");
                 response = Console.ReadLine();
             }
-            Console.Write("Learning rate is {0}. Choose a # of iterations [For best results, a minimum of 5000 is recommended]: ", alpha);
+            if (alpha > 0.3)
+            {
+                Console.WriteLine("Ok, a little big, don't you think? Let's just stick with 0.01.");
+                alpha = 0.01;
+            }
+            if (alpha < 0.0000001)
+            {
+                Console.WriteLine("Ok, a little small, don't you think? Let's just stick with 0.000001.");
+                alpha = 0.000001;
+            }
+            Console.Write("\nLearning rate is {0}. Choose a # of iterations [For best results, a minimum of 5000 is recommended]: ", alpha);
             response = Console.ReadLine();
             while (!int.TryParse(response, out numIterations))
             {
                 Console.Write("Input was not an integer. Try again, choose a number of iterations: ");
                 response = Console.ReadLine();
+            }
+            if (numIterations < 500)
+            {
+                Console.WriteLine(numIterations + " is a pretty small number of iterations. Let's stick with 1000.");
+                numIterations = 1000;                
+            }
+            if (numIterations > 1000000)
+            {
+                Console.WriteLine("Capping number of iterations at a million.");
+                numIterations = 1000000;
             }
             Regression();       
         }
@@ -88,10 +130,19 @@ namespace GradDesc
                 }                
                 Console.WriteLine("theta0: {0}\ttheta1: {1}\ntheta2: {2}\terror: {3}", theta0, theta1, theta2, errorVal);              
             }
-            string bestmodel = "Best model: ";           
-            bestmodel += (Math.Round(theta2) != 0) ? Math.Round(theta2) + "x^2 + " : "";
-            bestmodel += (Math.Round(theta1) != 0) ? Math.Round(theta1) + "x + " : "";
-            bestmodel += (Math.Round(theta0) != 0) ? Math.Round(theta0) + "" : "0";
+            string bestmodel = "Best model: ";
+            theta2 = Math.Round(theta2);
+            theta1 = Math.Round(theta1);
+            theta0 = Math.Round(theta0);
+            if (theta2 != 1) //for the aesthetics of x^2 instead of 1x^2
+                bestmodel += (theta2 != 0) ? theta2 + "x^2 + " : ""; //exclude if not applicable
+            else
+                bestmodel += "x^2 + ";
+            if (theta2 != 1)
+                bestmodel += (theta1 != 0) ? theta1 + "x + " : "";
+            else
+                bestmodel += "x + ";            
+            bestmodel += (theta0 != 0) ? theta0 + "" : "0";
             Console.WriteLine(bestmodel);
             Console.WriteLine("Average error rate: " + errorVal / Xs.Count);
             Console.WriteLine("Press enter if you want to apply linear regression to another data file!");
